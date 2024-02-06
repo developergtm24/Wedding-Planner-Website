cda17c5405f7a386bb47f2d5f6f4342d15bb9cac
Goutam prajapat <155313517+developergtm24@users.noreply.github.com>
2024-02-06 11:05:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let menu = document.querySelector('#menu-bar');
+let head = document.querySelector('.head .navbar');
+
+menu.onclick = () => {
+    head.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    head.classList.remove('active');
+    if (window.scrollY > 60) {
+        document.querySelector('#menu-bar').classList.add('active');
+    } else {
+        document.querySelector('#menu-bar').classList.remove('active');
+    }
+};
